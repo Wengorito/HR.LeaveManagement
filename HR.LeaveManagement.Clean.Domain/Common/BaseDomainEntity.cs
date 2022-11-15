75987b1544500b572b92bda13f57cec47d0e5c32
Grzegorz Kowalski <grzegorz.kowalski@pcosa.com.pl>
2022-11-15 13:22:27 +0100

    Added Persistence and Infrastructure blocks. Created DbContext with automated audit information logging to database (LINQ filtered).

--- HR.LeaveManagement.Clean.Domain/Common/BaseDomainEntity.cs
+++ HR.LeaveManagement.Clean.Domain/Common/BaseDomainEntity.cs
@@ -3,7 +3,7 @@
     public abstract class BaseDomainEntity
     {
         public int Id { get; set; }
-        public DateTime CreateDate { get; set; }
+        public DateTime CreatedDate { get; set; }
         public string? CreatedBy { get; set; }
         public DateTime ModifiedDate { get; set; }
         public string? ModifiedBy { get; set; }
